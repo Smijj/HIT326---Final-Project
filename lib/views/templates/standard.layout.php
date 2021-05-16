4c74a09d080e03a59c314913eeb0c00cb1af766e
TheSheeponator <swhume12@gmail.com>
2021-05-16 19:28:46 +0930

    + Temp bootstrap CSS

--- lib/views/templates/standard.layout.php
+++ lib/views/templates/standard.layout.php
@@ -4,21 +4,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link to external CSS for quick and easy prototyping. -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <title><?php if (!empty($title)) { echo $title; } ?></title>
 </head>
 <body>
-    <header>
-    <?php
-        if (!empty($is_auth) && $is_auth === true) {
-            $btLocation = "/signout";
-            $btName = "sign out";
-        } else {
-            $btLocation = "/signin";
-            $btName = "sign in";
-        }
-        echo "<div class='login'><a href='{$btLocation}'><input type='button' value='{$btName}'/></a></div>";
-    ?>
+    <header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+        </form>
+
+        <div class="text-end">
+            <?php
+                if (!empty($is_auth) && $is_auth === true) {
+                    echo "<a href='/signout'><button type='button' class='btn btn-warning'>Sign-out</button></a>";
+                } else {
+                    echo "<a href='/signin'><button type='button' class='btn btn-outline-light me-2'>Sign-in</button></a>";
+                }
+            ?>
+        </div>
+      </div>
+    </div>
     </header>
     <?php if (!empty($content)) { require $content; } ?>
 </body>
+<!-- Link to external JS for quick and easy CSS/visual prototyping. -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </html>
