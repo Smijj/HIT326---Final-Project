350ade417b526bb6ad5d0a924ff3987e23d0e898
TheSheeponator <swhume12@gmail.com>
2021-05-22 17:38:30 +0930

    + no last nam support with '.'

--- lib/views/partials/articleForm.php
+++ lib/views/partials/articleForm.php
@@ -1,7 +1,7 @@
-<label for="author">Author ID: <?php echo "{$author_id} \n"; ?></label><br>
+<label for="author">Author: <?php echo "{$name} \n"; ?></label><br>
 <label for="title">Title:</label>
 <input type="title" name="title" id="title" value="<?php if (isset($title)) {echo $title;} ?>" autocomplete="title"><br>
 <label for="keywords">Keywords:</label>
 <input type="keywords" name="keywords" id="keywords" value="<?php if (isset($keywords)) {echo $keywords;} ?>" autocomplete="keywords"><br>
-<label for="article_content">Content:</label>
-<textarea type="article_content" name="article_content" id="article_content" value="<?php if (isset($article_content)) {echo $article_content;} ?>" autocomplete="article_content"></textarea><br>
+<label for="article_content">Content:</label><br/>
+<textarea type="article_content" name="article_content" id="article_content" value="<?php if (isset($article_content)) {echo $article_content;} ?>" autocomplete="article_content" style="min-width:70%;"></textarea><br>
