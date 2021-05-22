ec9d6c9ba6347d44406555f4f25df53ad589654d
Smijj <slyer764@hotmail.com>
2021-05-22 18:31:24 +0930

    Added basic layout for article list page

--- lib/views/templates/articlelist.php
+++ lib/views/templates/articlelist.php
@@ -9,7 +9,16 @@
 <?php 
 
     foreach ($article_list as $article => $article_data) {
-        echo "<div>{$article_data->title}</div>";
+        echo "<article>
+            <h2 class='article_list_header'>{$article_data->title}</h2>
+            <p class='article_list_subheader'>Written by {$article_data->name}</p>
+            <p class='article_list_subheader'>Last Edited on {$article_data->date_last_edit}</p>
+            <div class='article_list_content'>
+            <p class='article_list_keywords'>Keywords: {$article_data->keywords}</p>
+            {$article_data->content}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, aperiam nisi, voluptatum assumenda cum repellendus ratione natus ipsa, illo consectetur atque! Nemo quis repellendus ipsam!
+            </div>
+        </article>";
     }
 
 ?>
