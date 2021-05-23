64c96f399cd50e534b1bcdd33b91946c1c97d5a1
Smijj <slyer764@hotmail.com>
2021-05-23 20:21:01 +0930

    +CSS's main page and article list page to display articles properly while maintaining the ability to click on the article, the edit button, and the delete button without any of them overiding the other.

--- lib/views/templates/articlelist.php
+++ lib/views/templates/articlelist.php
@@ -4,7 +4,7 @@
     } 
 ?>
 
-<p>This is the article list page</p>
+<h1 class='article_list_header'>Articles</h1>
 
 <?php 
 
@@ -12,17 +12,17 @@
         // Show article in list if the article is public or the current logged in user has high enough permissions
         if ($article_data->public == true || $high_level_user == true) {
             echo "<article class='article_list'>
-                <div  data-href=\"/article/{$article_data->article_id}\">
-                <h2 class='article_list_header'>{$article_data->title}</h2>
-                <p class='article_list_subheader'>Written by {$article_data->name}</p>
-                <p class='article_list_subheader'>Last Edited on {$article_data->date_last_edit}</p>
-                <p class='article_list_keywords'>Keywords: {$article_data->keywords}</p>
-                </div>";
+                <section  class='article_list_info' data-href=\"/article/{$article_data->article_id}\">
+                    <h2 class='article_list_header'>{$article_data->title}</h2>
+                    <p class='article_list_subheader'>Written by {$article_data->name}</p>
+                    <p class='article_list_subheader'>Last Edited on {$article_data->date_last_edit}</p>
+                    <p class='article_list_keywords'>Keywords: {$article_data->keywords}</p>
+                </section>";
             if ($high_level_user == true) {
-                echo "<div class='article_list_buttons'>
+                echo "<section class='article_list_buttons'>
                         <div data-href='/editarticle/{$article_data->article_id}'>Edit</div>
                         <div data-href='#'>Delete</div>
-                    </div>
+                    </section>
                 </article>";
             } else {
                 echo "</article>";
--- lib/views/templates/mainpage.php
+++ lib/views/templates/mainpage.php
@@ -1,19 +1,23 @@
 <?php
     if (!empty($flash)) { echo "<div class='flash'>{$flash}</div>"; }
 ?>
+
+<h1 class='article_list_header'>Recent Articles</h1>
+
 <div class="topfive">
-<h3>Recent Articles</h3>
     <?php
     if (!empty($top5articles)) {
         foreach ($top5articles as $article => $article_data) {
             // Show article in list if the article is public.
             if ($article_data->public == true) {
-                echo "<article class='article_list' data-href=\"/article/{$article_data->article_id}\">
-                            <h2 class='article_list_header'>{$article_data->title}</h2>
-                            <p class='article_list_subheader'>Written by {$article_data->name}</p>
-                            <p class='article_list_subheader'>Last Edited on {$article_data->date_last_edit}</p>
-                            <p class='article_list_keywords'>Keywords: {$article_data->keywords}</p>";
-                echo "</article>";
+                echo "<article class='article_list'>
+                    <section  class='article_list_info' data-href=\"/article/{$article_data->article_id}\">
+                        <h2 class='article_list_header'>{$article_data->title}</h2>
+                        <p class='article_list_subheader'>Written by {$article_data->name}</p>
+                        <p class='article_list_subheader'>Last Edited on {$article_data->date_last_edit}</p>
+                        <p class='article_list_keywords'>Keywords: {$article_data->keywords}</p>
+                    </section>
+                </article>";
             }
         }
     }
